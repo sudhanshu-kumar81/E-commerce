28d81e1da21fb6f61399b0939f8ab7a8a92de646
sudhanshu-kumar81 <sudhanshukumar8181@gmail.com>
2024-04-10 01:20:55 +0530

    third commit

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ProductDetails from '../features/product-list/ProductDetails'
+import Navbar from '../features/navbar/Navbar'
 
 const ProductDetailsPage = () => {
   return (
     <div>
-      <ProductDetails/>
+        <Navbar>
+        <ProductDetails/>
+        </Navbar>
+     
     </div>
   )
 }
