18ceb3fc8f83086a0ad8dfdb2ab86a57d67435b6
sudhanshu-kumar81 <sudhanshukumar8181@gmail.com>
2024-04-13 00:48:11 +0530

    fourth commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
+// import counterReducer from '../features/counter/counterSlice'
 import productListReducer from '../features/product-list/productlistSlice'
 export const store = configureStore({
   reducer: {
-   counter:counterReducer,
-   productList:productListReducer
+  //  counter:counterReducer,
+   product:productListReducer
   },
 })
--- src/features/product-list/productlistSlice.js
+++ src/features/product-list/productlistSlice.js
@@ -1,30 +1,57 @@
-import { createSlice } from '@reduxjs/toolkit'
-
-export const productListSlice = createSlice({
-  name: 'productList',
-  initialState: {
-    value: 0,
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { fetchAllProducts ,fetchAllProductsByFilter} from './productAPI.JS'
+// First, create the thunk
+export const fetchAllProductsAsync = createAsyncThunk(
+  'product/fetchAllProducts',
+  async () => {
+    const response = await fetchAllProducts();
+    console.log("response from fetchAllProductsAsync is ",response);
+    return response.data
   },
+)
+export const fetchAllProductsByFilterAsync = createAsyncThunk(
+  'product/fetchAllProductsByFilter',
+  async (filter) => {
+    const response = await fetchAllProductsByFilter(filter);
+    console.log("response from fetchAllProductsAsync is ",response);
+    return response.data
+  },
+)
+
+const initialState = {
+  products: [],
+  status: 'idle',
+}
+
+// Then, handle actions in your reducers:
+export const productSlice = createSlice({
+  name: 'product',
+  initialState,
   reducers: {
     increment: (state) => {
-      state.value += 1
+      state.value += 1;
     },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload
-    },
+    // standard reducer logic, with auto-generated action types per reducer
+  },
+  extraReducers: (builder) => {
+    
+    builder.addCase(fetchAllProductsAsync.pending, (state) => {
+      state.status='loading'
+      })
+      .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
+      state.status='idle'
+      state.products=action.payload
+    })
+    .addCase(fetchAllProductsByFilterAsync.pending, (state) => {
+      state.status='loading'
+      })
+      .addCase(fetchAllProductsByFilterAsync.fulfilled, (state, action) => {
+      state.status='idle'
+      state.products=action.payload
+    })
+    
   },
 })
-
-export const { increment, decrement, incrementByAmount } = productListSlice.actions
-
-export const incrementAsync = (amount) => (dispatch) => {
-  setTimeout(() => {
-    dispatch(incrementByAmount(amount))
-  }, 1000)
-}
-export const selectCount = (state) => state.productList.value
-
-export default productListSlice.reducer
+export const { increment } = productSlice.actions;
+export const selectAllProducts=(state)=>state.product.products
+export default productSlice.reducer
